e8a8b0180f9159a5e655549c475addaf4d577b81
zsolt13 <zsolt@bereadysoftware>
2019-10-14 16:26:38 +0200

    Initial Commit

---
+++ Alltodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alltodo
+//
+//  Created by zsolt on 14/10/2019.
+//  Copyright © 2019 zsolt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
